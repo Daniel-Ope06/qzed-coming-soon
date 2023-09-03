9a742b224982adb9f91c950007df695bfef645aa
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-04 00:55:53 +0100

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from './services/theme-switch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'qzed-app';
+export class AppComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
+
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
