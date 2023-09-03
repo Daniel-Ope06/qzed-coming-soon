5fd6e1ec3dc50accb935a59985807ff620c166ca
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-03 21:50:31 +0100

    Set up theme-switch service

--- src/app/services/theme-switch.service.ts
+++ src/app/services/theme-switch.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeSwitchService {
+  private isDarkThemeSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public isDarkTheme$: Observable<boolean> = this.isDarkThemeSubject.asObservable();
 
   constructor() { }
+
+  setTheme(isDark: boolean): void {
+    this.isDarkThemeSubject.next(isDark);
+  }
+
+  getTheme(): boolean {
+    return this.isDarkThemeSubject.getValue();
+  }
 }
