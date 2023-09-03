dc912b7387e9c8afd86067740469a5a6330ffc24
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-03 21:54:11 +0100

    Create theme-switch component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// common components
+import { ThemeSwitchComponent } from './components/common/theme-switch/theme-switch.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    // common
+    ThemeSwitchComponent
   ],
   imports: [
     BrowserModule,
