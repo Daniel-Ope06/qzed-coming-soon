23a52fabcdfdb9f046c643ae662ca488d3d86a15
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-03 22:04:40 +0100

    Set up theme-switch component

--- src/app/components/common/theme-switch/theme-switch.component.ts
+++ src/app/components/common/theme-switch/theme-switch.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-theme-switch',
   templateUrl: './theme-switch.component.html',
   styleUrls: ['./theme-switch.component.scss']
 })
-export class ThemeSwitchComponent {
+export class ThemeSwitchComponent implements OnInit {
+  isDarkTheme = false;
+  themeStorage = 'qzed-theme';
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void { this.initializeTheme(); }
+
+  toggleTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.themeSwitchService.setTheme(this.isDarkTheme);
+    localStorage.setItem(this.themeStorage , this.isDarkTheme ? 'dark' : 'light');
+  }
+
+  initializeTheme(): void {
+    if (localStorage.getItem(this.themeStorage)) {
+      this.isDarkTheme = localStorage.getItem(this.themeStorage) === 'dark' ? true : false;
+      this.themeSwitchService.setTheme(this.isDarkTheme);
+    } else {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+      this.isDarkTheme = prefersDark.matches;
+      this.themeSwitchService.setTheme(this.isDarkTheme);
+    }
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+      localStorage.setItem(this.themeStorage, isDarkTheme ? 'dark' : 'light');
+    });
+  }
 }
