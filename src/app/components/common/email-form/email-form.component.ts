f9c66778dd51e329f490b7f40c6fd000a68f891c
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-04 10:39:32 +0100

    Set up email-form component

--- src/app/components/common/email-form/email-form.component.ts
+++ src/app/components/common/email-form/email-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-email-form',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./email-form.component.scss']
 })
 export class EmailFormComponent {
+  emailInput: string = '';
+  isEmailExpanded: boolean = false;
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
+
+  openWhatsApp(): void {
+    window.open('https://chat.whatsapp.com/B4iok9Lpl0SL6C7MhR2Hhn', '_blank');
+  }
+
+  toggleEmail(): void {
+    this.isEmailExpanded = !this.isEmailExpanded;
+  }
+
+  isEmailValid(): boolean {
+    return (this.emailInput.includes('@') && this.emailInput.includes('.') );
+  }
 }
