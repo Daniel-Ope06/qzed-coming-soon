eac606aa2ce0a603fd98fad3b96b0ae6fae363c1
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-04 11:52:09 +0100

    Set up coming-soon-desktop

--- src/app/pages/desktop/coming-soon-desktop/coming-soon-desktop.component.ts
+++ src/app/pages/desktop/coming-soon-desktop/coming-soon-desktop.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-coming-soon-desktop',
   templateUrl: './coming-soon-desktop.component.html',
   styleUrls: ['./coming-soon-desktop.component.scss']
 })
-export class ComingSoonDesktopComponent {
+export class ComingSoonDesktopComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
