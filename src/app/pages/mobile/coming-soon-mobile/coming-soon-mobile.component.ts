fc854e2e303a4a067207c96343dadf3e45dca5af
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-05 11:46:11 +0100

    Set up coming-soon-mobile

--- src/app/pages/mobile/coming-soon-mobile/coming-soon-mobile.component.ts
+++ src/app/pages/mobile/coming-soon-mobile/coming-soon-mobile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-coming-soon-mobile',
   templateUrl: './coming-soon-mobile.component.html',
   styleUrls: ['./coming-soon-mobile.component.scss']
 })
-export class ComingSoonMobileComponent {
+export class ComingSoonMobileComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
