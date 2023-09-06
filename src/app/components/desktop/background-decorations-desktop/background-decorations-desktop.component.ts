68559f6aa925aad72f28a1cc0409446a81935511
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-06 11:05:50 +0100

    Set up background-decorations-desktop

--- src/app/components/desktop/background-decorations-desktop/background-decorations-desktop.component.ts
+++ src/app/components/desktop/background-decorations-desktop/background-decorations-desktop.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-background-decorations-desktop',
   templateUrl: './background-decorations-desktop.component.html',
   styleUrls: ['./background-decorations-desktop.component.scss']
 })
-export class BackgroundDecorationsDesktopComponent {
+export class BackgroundDecorationsDesktopComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
