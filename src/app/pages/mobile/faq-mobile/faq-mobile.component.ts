368304c3d77588bc9149db1078a35ec93c5226b0
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-07 12:05:02 +0100

    Set up faq mobile page

--- src/app/pages/mobile/faq-mobile/faq-mobile.component.ts
+++ src/app/pages/mobile/faq-mobile/faq-mobile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-faq-mobile',
   templateUrl: './faq-mobile.component.html',
   styleUrls: ['./faq-mobile.component.scss']
 })
-export class FaqMobileComponent {
+export class FaqMobileComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
