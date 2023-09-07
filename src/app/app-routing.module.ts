311ce2f051af3e34d9f47164cf2932b8b2cbdb7c
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-07 12:04:37 +0100

    Add faq route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ComingSoonComponent } from './pages/combined/coming-soon/coming-soon.component';
+import { FaqComponent } from './pages/combined/faq/faq.component';
 
 const routes: Routes = [
   {path:'coming-soon', title: 'QZed | Coming Soon', component: ComingSoonComponent},
+  {path:'faq', title: 'QZed | FAQ', component: FaqComponent},
   {path:'', redirectTo: 'coming-soon', pathMatch: 'full'}
 ];
 
