3ef42e0e5f964fefb2ec11a4d34bdeac876edac4
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-07 21:00:13 +0100

    Set up faq-desktop

--- src/app/pages/desktop/faq-desktop/faq-desktop.component.ts
+++ src/app/pages/desktop/faq-desktop/faq-desktop.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitchService } from 'src/app/services/theme-switch.service';
 
 @Component({
   selector: 'app-faq-desktop',
   templateUrl: './faq-desktop.component.html',
   styleUrls: ['./faq-desktop.component.scss']
 })
-export class FaqDesktopComponent {
+export class FaqDesktopComponent implements OnInit {
+  isDarkTheme: boolean = this.themeSwitchService.getTheme();
 
+  constructor(private themeSwitchService: ThemeSwitchService) { }
+
+  ngOnInit(): void {
+    this.themeSwitchService.isDarkTheme$.subscribe((isDarkTheme: boolean) => {
+      this.isDarkTheme = isDarkTheme;
+    });
+  }
 }
