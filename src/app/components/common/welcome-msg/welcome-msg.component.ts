5b637819fe6c6c6d24fce17279ffc4fd2ca4528f
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-09-15 13:58:15 +0100

    Set up invite button to share to WhatsApp

--- src/app/components/common/welcome-msg/welcome-msg.component.ts
+++ src/app/components/common/welcome-msg/welcome-msg.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-msg.component.scss']
 })
 export class WelcomeMsgComponent {
+  inviteMessage: string = "*Join us on QZed* - the ultimate academic Q&A platform.\nBe the first to know when we launch!\n\n*Subscribe here*: https://qzedanswers.web.app/";
 
+  sendWhatsAppMessage(): void {
+    const encodedMessage = encodeURIComponent(this.inviteMessage);
+    const whatsappLink = `https://wa.me/?text=${encodedMessage}`;
+    window.open(whatsappLink, '_blank');
+  }
 }
